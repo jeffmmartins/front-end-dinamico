99c3be5bec07eea7eb3f883280b26b30532a728c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-04 09:36:49 -0300

    js: exemplo de getelementbytagname

---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log(document.title)
+console.log(document.url)
+
+//Selecionar elementos da nossa página 
+const todoH1 = document.getElementsByTagName('h1')
+console.log(todoH1) // aparece com um array , consigo acessar pela posição 
