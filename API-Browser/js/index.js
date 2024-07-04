01b7c4549cd1aca734dff1d2ffc790d2232c152a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-04 16:18:05 -0300

    API: criando um evento addEventListener

---
+++ API-Browser/js/index.js
@@ -0,0 +1,9 @@
+const buttonAumentar = document.querySelector('#aumentar-botao')
+const buttonDiminuir = document.querySelector('#diminuir-botao')
+const contadorElemento  = document.querySelector('#contador')
+// adicionando eventos 
+// target é quem disparou o evento
+// quando esse evento acontecer uma função sera disparada
+button.addEventListener('click', (event) => {
+
+})
